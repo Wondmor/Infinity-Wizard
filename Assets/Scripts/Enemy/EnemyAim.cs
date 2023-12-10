7969e5c1a04c77a58822c11eb0fd3436d646f309
Wondmor <Wza87656127@gmail.com>
2023-12-10 21:55:13 +0800

    fix

---
+++ Assets/Scripts/Enemy/EnemyAim.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+public class EnemyAim : MonoBehaviour
+{
+    private GameObject player;
+
+    private void OnEnable()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        player = GameObject.FindWithTag("Player");
+        Vector2 playerPositon = new Vector2(player.transform.position.x, player.transform.position.y);
+        Vector2 thisgam = new Vector2(this.transform.position.x, this.transform.position.y);
+        float angle = anglewhat(playerPositon, thisgam);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    
+    private float anglewhat(Vector2 a, Vector2 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
